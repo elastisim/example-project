54bdfee45395c670bad1bc2bb8c00191c31466f3
Taylan Özden <65979060+oezden@users.noreply.github.com>
2023-11-07 21:03:11 +0100

    Update example project to include evolving and adaptive jobs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ElastiSim Example Project
 
-An example ElastiSim project utilizing an FCFS algorithm (without backfill) applied on 32 rigid jobs with alternating compute and I/O phases running on a crossbar topology with 128 compute nodes.
+This scenario simulates an FCFS (first come, first serve) scheduling algorithm applied on 1000 jobs—including all job types—with alternating compute and I/O phases running on a crossbar topology with 128 compute nodes. While evolving and adaptive jobs request a new configuration at the beginning of each iteration of the specified phase, malleable jobs accept any reconfiguration. The scheduler accepts all evolving requests that shrink the job but accepts evolving requests to expand the job only to a maximum of available nodes. For malleable jobs, the scheduler expands to the highest possible number of nodes based on the number of available nodes. The following steps will create a Docker container including all the required libraries for ElastiSim and start the simulation.
 
 ## Installation
 
@@ -13,7 +13,7 @@ docker build -t elastisim .
 
 To run the simulation, execute the following commands in two different sessions:
 
-### \*nix:
+### Linux:
 ```sh
 docker run -v $PWD/data:/data -v $PWD/algorithm:/algorithm -u `id -u $USER` --name elastisim -it --rm elastisim /data/input/configuration.json --log=root.thresh:warning
 docker exec -u `id -u $USER` -it elastisim python3 /algorithm/algorithm.py
@@ -35,6 +35,6 @@ The first command runs the ElastiSim simulator process and accepts two inputs:
 - the configuration file (JSON)
 - the logging level
 
-For a more detailed output change `--log=root.thresh:warning` to `--log=root.thresh:info` (caution: verbose).
+For a more detailed output, change `--log=root.thresh:warning` to `--log=root.thresh:info` (caution: verbose).
 
 The second command runs the scheduling algorithm.
--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -5,8 +5,7 @@
 # This software may be modified and distributed under the terms of the 3-Clause
 # BSD License. See the LICENSE file in the base directory for details.
 from typing import Any
-
-from elastisim_python import JobState, JobType, NodeState, pass_algorithm, Job, Node
+from elastisim_python import JobState, JobType, NodeState, pass_algorithm, Job, Node, InvocationType
 
 
 def schedule(jobs: list[Job], nodes: list[Node], system: dict[str, Any]) -> None:
@@ -23,23 +22,36 @@ def schedule(jobs: list[Job], nodes: list[Node], system: dict[str, Any]) -> None
     allocated_nodes = [node for node in nodes if node.state == NodeState.ALLOCATED]
     reserved_nodes = [node for node in nodes if node.state == NodeState.RESERVED]
 
-    for job in jobs:
-        if not free_nodes:
-            break
-        if (job.type == JobType.MOLDABLE or job.type == JobType.MALLEABLE) and job.state == JobState.PENDING:
-            num_nodes_to_assign = min(len(free_nodes), job.num_nodes_max)
-            if num_nodes_to_assign >= job.num_nodes_min:
-                job.assign(free_nodes[:num_nodes_to_assign])
-                del free_nodes[:num_nodes_to_assign]
-        elif job.type == JobType.MALLEABLE and job.state == JobState.RUNNING:
-            num_nodes_to_expand = min(len(free_nodes), job.num_nodes_max - len(job.assigned_nodes))
-            if num_nodes_to_expand > 0:
-                job.assign(free_nodes[:num_nodes_to_expand])
-                del free_nodes[:num_nodes_to_expand]
-        elif job.type == JobType.RIGID and job.state == JobState.PENDING:
-            if job.num_nodes <= len(free_nodes):
-                job.assign(free_nodes[:job.num_nodes])
-                del free_nodes[:job.num_nodes]
+    if system['invocation_type'] == InvocationType.INVOKE_SCHEDULING_POINT:
+        job = system['job']
+        num_nodes_to_expand = min(len(free_nodes), job.num_nodes_max - len(job.assigned_nodes))
+        if num_nodes_to_expand > 0:
+            job.assign(free_nodes[:num_nodes_to_expand])
+            del free_nodes[:num_nodes_to_expand]
+    elif system['invocation_type'] == InvocationType.INVOKE_EVOLVING_REQUEST:
+        job = system['job']
+        evolving_request = system['evolving_request']
+        num_nodes = len(job.assigned_nodes)
+        diff = evolving_request - num_nodes
+        if diff < 0:
+            job.remove(job.assigned_nodes[diff:])
+        elif len(free_nodes) >= diff:
+            job.assign(free_nodes[:diff])
+    else:
+        for job in pending_jobs:
+            if job.type == JobType.RIGID:
+                if job.num_nodes <= len(free_nodes):
+                    job.assign(free_nodes[:job.num_nodes])
+                    del free_nodes[:job.num_nodes]
+                else:
+                    break
+            else:
+                num_nodes_to_assign = min(len(free_nodes), job.num_nodes_max)
+                if num_nodes_to_assign >= job.num_nodes_min:
+                    job.assign(free_nodes[:num_nodes_to_assign])
+                    del free_nodes[:num_nodes_to_assign]
+                else:
+                    break
 
 
 if __name__ == '__main__':
